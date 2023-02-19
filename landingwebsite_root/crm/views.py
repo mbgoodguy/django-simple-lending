7750fbf67ff1d845cc57d8b14cbcece3b24d0950
mbgoodguy <nerooren4001@gmail.com>
2023-02-19 14:41:30 +0300

    2023-02-19 14:41

--- landingwebsite_root/crm/views.py
+++ landingwebsite_root/crm/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Order
+from .forms import OrderForm
 
 
 # Create your views here.
 def first_page(request):
+    form = OrderForm()
     object_list = Order.objects.all()
-    return render(request, './index.html', {'object_list': object_list})
+    return render(request, './index.html', {'object_list': object_list,
+                                            'form': form
+                                            })
 
 
 def thanks_page(request):
