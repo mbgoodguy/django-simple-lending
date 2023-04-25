d30e8609fbfe89d7757d516d076af4804e5f6d6f
mbgoodguy <nerooren4001@gmail.com>
2023-04-25 15:49:54 +0300

    added STATIC_ROOT

--- landingwebsite_root/landingwebsite/settings.py
+++ landingwebsite_root/landingwebsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,6 +122,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
