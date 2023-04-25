7552c994042506fe44f3396df0ec5aeddd08dc02
mbgoodguy <nerooren4001@gmail.com>
2023-04-25 20:14:20 +0300

    added carusel, base, price, thanks

--- landingwebsite_root/landingwebsite/settings.py
+++ landingwebsite_root/landingwebsite/settings.py
@@ -118,11 +118,13 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     BASE_DIR / "landingwebsite/static/",
-    '/var/www/static/',
 ]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
