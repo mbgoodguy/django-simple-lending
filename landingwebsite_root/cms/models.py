36d13134326f6eb0a88b68f8a0e5c4b9b0257acc
mbgoodguy <nerooren4001@gmail.com>
2023-05-09 13:41:08 +0300

    changed CmsSlider

--- landingwebsite_root/cms/models.py
+++ landingwebsite_root/cms/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class CmsSlider(models.Model):
     cms_img = models.ImageField(upload_to='sliderimg/')
     cms_title = models.CharField(max_length=200, verbose_name='Заголовок')
-    cms_text = models.CharField(max_length=200, verbose_name='Заголовок')
+    cms_text = models.CharField(max_length=200, verbose_name='Текст')
 
     def __str__(self):
-        return self.order_name
+        return self.cms_title
 
     class Meta:
-        verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
+        verbose_name = 'Слайд'
+        verbose_name_plural = 'Слайды'
