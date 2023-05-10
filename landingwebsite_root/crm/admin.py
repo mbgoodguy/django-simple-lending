0c5af312b94bd2930eae3e07b9598a9e0cae7508
mbgoodguy <nerooren4001@gmail.com>
2023-05-10 17:14:00 +0300

    registred new model ComentCrm

--- landingwebsite_root/crm/admin.py
+++ landingwebsite_root/crm/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Order, StatusCrm
+from .models import Order, StatusCrm, ComentCrm
 
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(StatusCrm)
+admin.site.register(ComentCrm)
