3a9c42378534b7106251875c961d51cc5a4d5607
mbgoodguy <nerooren4001@gmail.com>
2023-05-10 17:13:17 +0300

    added new class ComentCrm

--- landingwebsite_root/crm/models.py
+++ landingwebsite_root/crm/models.py
@@ -1,21 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
-
-
-class Order(models.Model):
-    order_dt = models.DateTimeField(auto_now=True)
-    order_name = models.CharField(max_length=200, verbose_name='Имя')
-    order_phone = models.CharField(max_length=200, verbose_name='Телефон')
-
-    def __str__(self):
-        return self.order_name
-
-    class Meta:
-        verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
-
-
 class StatusCrm(models.Model):
     status_name = models.CharField(max_length=50, verbose_name='Статус заявки')
 
@@ -25,3 +11,30 @@ class StatusCrm(models.Model):
     class Meta:
         verbose_name = 'Статус'
         verbose_name_plural = 'Статусы'
+
+
+class Order(models.Model):
+    order_dt = models.DateTimeField(auto_now=True)
+    order_name = models.CharField(max_length=200, verbose_name='Имя')
+    order_phone = models.CharField(max_length=200, verbose_name='Телефон')
+    order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
+
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class ComentCrm(models.Model):
+    coment_binding = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заявка')
+    coment_text = models.TextField(verbose_name='Текст комментария')
+    coment_dt = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.coment_text
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
