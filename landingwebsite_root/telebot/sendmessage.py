f245023f1370acee2c14d810ed6b4d697782aa5b
mbgoodguy <nerooren4001@gmail.com>
2023-05-15 16:26:30 +0300

    Add try, finally blocks

--- landingwebsite_root/telebot/sendmessage.py
+++ landingwebsite_root/telebot/sendmessage.py
@@ -10,19 +10,29 @@ def sendtelegram(tg_name, tg_phone):
         text = str(settings.tg_message)
         api = 'https://api.telegram.org/bot'
         method = api + token + '/sendMessage'
-
         if text.find('{') and text.find('}') and text.rfind('{') and text.rfind('}'):
             part1 = text[0:text.find('{')]
             part2 = text[text.find('}')+1:text.rfind('{')]
             part3 = text[text.rfind('}'):-1]
-
             text_slice = part1 + tg_name + part2 + tg_phone + part3
         else:
             text_slice = text
 
-        req = requests.post(method, data={
-            'chat_id': chat_id,
-            'text': text_slice,
-        })
+        try:
+            req = requests.post(method, data={
+                'chat_id': chat_id,
+                'text': text_slice,
+            })
+        except:
+            pass
+
+        finally:
+            if req.status_code != 200:
+                print('Ошибка отправки!')
+            elif req.status_code == 500:
+                print('Ошибка 500')
+            else:
+                print('Все ок, сообщение отправлено!')
+
     else:
         pass
