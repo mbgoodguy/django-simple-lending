0923cdc82b0585c07cc28bfc45f5634b991487f7
mbgoodguy <nerooren4001@gmail.com>
2023-05-15 16:03:56 +0300

    Add check for request existing

--- landingwebsite_root/crm/views.py
+++ landingwebsite_root/crm/views.py
@@ -26,11 +26,15 @@ def first_page(request):
 
 
 def thanks_page(request):
-    name = request.POST['name']
-    phone = request.POST['phone']
-    element = Order(order_name=name, order_phone=phone)
-    element.save()
-    sendtelegram(tg_name=name, tg_phone=phone)
-    return render(request, './thanks.html', {'name': name, 'phone': phone})
+    if request.POST:
+        name = request.POST['name']
+        phone = request.POST['phone']
+        element = Order(order_name=name, order_phone=phone)
+        element.save()
+        sendtelegram(tg_name=name, tg_phone=phone)
+        return render(request, './thanks.html', {'name': name, 'phone': phone})
+    else:
+        return render(request, './thanks.html')
+
 
 
